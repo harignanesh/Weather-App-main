c5a36a194a67590f9b42132b1f9c85430bf572ca
harig <harig@DESKTOP-5UJN7V5>
2025-02-21 11:14:51 +0530

    change file name

--- script.js
+++ script.js
@@ -46,7 +46,6 @@ let oldTab = userTab;
 const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
 oldTab.classList.add("current-tab");
 getfromSessionStorage();
-fetchSearchWeatherInfo('chennai');
 function switchTab(newTab) {
     if(newTab != oldTab) {
         oldTab.classList.remove("current-tab");
